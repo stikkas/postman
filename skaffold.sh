e0672baa9629027b6e3482c66400912fe4144e54
sergey.blagodatskikh <sergey.blagodatskikh@x5.ru>
2024-02-14 11:46:45 +0300

    kafka is working

--- skaffold.sh
+++ skaffold.sh
@@ -5,3 +5,9 @@ eval $(minikube -p cux5 docker-env)
 
 # and run with port forwarding config of which is taken from skaffold.yaml
 skaffold run --port-forward=user
+
+# show images
+minikube image ls -p cux5 --format table
+
+# remove image
+minikube image rm e29cc5227753e -p cux5
